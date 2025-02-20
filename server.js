d72b0ffee6e91662878bd5bce1176981e93a948d
Matvei <matvejsarov673@yandex.com>
2025-02-20 09:54:15 +0300

    First commit base node_module

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(port, ()=> { 
+   console.log(`Server start in ${new Date} to http://localhost:${port}`);
+});
